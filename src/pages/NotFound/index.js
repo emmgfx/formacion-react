cccbbe0c906c6862c2e5fc2f7233af5330e3ea3a
Josep Viciana <emmgfx@gmail.com>
2021-01-15 13:14:50 +0100

    Día 3: Lifecycles / interval

--- src/pages/NotFound/index.js
+++ src/pages/NotFound/index.js
@@ -1,6 +1,25 @@
+import { useEffect, useState } from "react";
+
 const NotFound = props => {
+
+  const [seconds, setSeconds] = useState(0);
+
+  useEffect(() => {
+
+    // Cuando se monta:
+    const interval = setInterval(() => setSeconds(seconds => seconds+1), 1000);
+    
+    return () => {
+      // Cuando se desmonta:
+      clearInterval(interval);
+    }
+  }, []);
+
   return(
-    <h2>NotFound</h2>
+    <>
+      <h2>NotFound</h2>
+      <p>Has estado aquí {seconds}.</p>
+    </>
   );
 }
 export default NotFound;
